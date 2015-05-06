b8f20d059a3cdcbcc6c2cae1485b74011f6992f1
Torgeir <torgeha@gmail.com>
2015-05-06 11:05:32 +0200

    Changed environment to fit the task

--- environment/flatland.py
+++ environment/flatland.py
@@ -4,52 +4,15 @@ import math
 
 class World:
 
-    def __init__(self, board=None, dimension=(10, 10), fdr=(0.3, 0.3)):
+    def __init__(self, board, dimension):
         self.dimension = dimension
-        populate_now = False
-        if board == None:
-            self.board = [[0] * self.dimension[1] for x in range(self.dimension[0])]
-            populate_now = True
-        else:
-            self.board = board
-        self.board_length = len(self.board)
-
-        # Distribute food/posion based on fdr, food = 1, posion = -1
-        # TODO: more efficient way of doing this? distribute with some numpy shit while building grid?
-        nof_cells = dimension[0] * dimension[1]
-        self.nof_food = math.ceil(nof_cells * fdr[0])
-        self.nof_poison = math.ceil((nof_cells - self.nof_food) * fdr[1])
-
-        if populate_now:
-            self._populate_world()
-
-    def _populate_world(self):
-        # Agent startpos is always 9,9, dont place anyting there
-        for food in range(self.nof_food):
-            x, y = self.get_random_pos()
-
-            self.board[y][x] = 1
-
-            if food < self.nof_poison:
-                x, y = self.get_random_pos()
-                self.board[y][x] = 2
+        self.board = board
 
     def get_cell_content(self, x, y):
-        # TODO: handle torroidal
-        realx = x % self.board_length
-        realy = y % self.board_length
+        # Return content of cell. Supports world wrap-around.
+        realx = x % self.dimension[0]
+        realy = y % self.dimension[1]
         return self.board[realy][realx]
-
-    def get_random_pos(self):
-        # TODO: replace with some numpy shit
-        x, y = random.randint(0, (self.dimension[0] - 1)), random.randint(0, (self.dimension[0] - 1))
-        while True:
-            if self.board[y][x] == 0 and (y != 9 or x != 9):
-                # Nothing is already placed and startposition of agent is not touched.
-                break
-            x, y = random.randint(0, self.dimension[0] - 1), random.randint(0, self.dimension[0] - 1)
-
-        return x, y
 
     def __repr__(self):
         retval = ""
@@ -61,93 +24,51 @@ class World:
 
 class Agent:
 
-    def __init__(self, heading, world):
+    def __init__(self, world, start_position, goal_position):
         """
         Heading = 0,1,2,3 (n,e,s,w)
         """
-        self.start_pos = 9,9 # TODO: startposition is alwasy 9.9
+        self.start_pos = start_position
         self.pos = self.start_pos
-        self.heading = heading
+        self.goal_position = goal_position
         self.world = world
-        # self.board_length = len(board)
-        self.food_eaten = 0
-        self.poison_eaten = 0
-        self.visited = {}
 
+        # TODO: If same agent-instance for all learning scenarios, reset these
+        self.visited = {}
+        self.poison_eaten = 0
+        self.steps_taken = 0
+
+        # Update the visited map
         self._check_for_food_and_poison()
 
+    def reset(self):
+        # Can use same agent in many scenarios by resetting between each.
+
+        self.pos = self.start_pos
+        self.visited = {}
+        self.poison_eaten = 0
+        self.steps_taken = 0
 
     def move(self, direction):
-        """
-        direction=f,l,r
-        """
+        # direction = n,e,s,w / 0,1,2,3
+        # Returns board content in the cell it arrives at.
 
-        if direction == "f" or direction == 0:
-            self._move_forward() # No need to turn, move the way its heading
-        elif direction == "l" or direction == 1:
-            self.heading = (self.heading - 1) % 4
-            self._move_forward()
-        elif direction == "r" or direction == 2:
-            self.heading = (self.heading + 1) % 4
-            self._move_forward()
-
-    def get_inputs(self):
-        """
-        Return list range(6) with 1 or 0 depending on food/posion in all
-        """
-        inputs = [0] * 6
-        x = self.pos[0]
-        y = self.pos[1]
-
-        # inputs = [f,l,r,f,l,r], food - poison
-        if self.heading == 0:
-            inputs[0] = self._contains_food(x, y - 1)
-            inputs[1] = self._contains_food(x - 1, y)
-            inputs[2] = self._contains_food(x + 1, y)
-            inputs[3] = self._contains_poison(x, y - 1)
-            inputs[4] = self._contains_poison(x - 1, y)
-            inputs[5] = self._contains_poison(x + 1, y)
-        elif self.heading == 1:
-            inputs[0] = self._contains_food(x + 1, y)
-            inputs[1] = self._contains_food(x, y - 1)
-            inputs[2] = self._contains_food(x, y + 1)
-            inputs[3] = self._contains_poison(x + 1, y)
-            inputs[4] = self._contains_poison(x, y - 1)
-            inputs[5] = self._contains_poison(x, y + 1)
-        elif self.heading == 2:
-            inputs[0] = self._contains_food(x, y + 1)
-            inputs[1] = self._contains_food(x + 1, y)
-            inputs[2] = self._contains_food(x - 1, y)
-            inputs[3] = self._contains_poison(x, y + 1)
-            inputs[4] = self._contains_poison(x + 1, y)
-            inputs[5] = self._contains_poison(x - 1, y)
-        elif self.heading == 3:
-            inputs[0] = self._contains_food(x - 1, y)
-            inputs[1] = self._contains_food(x, y + 1)
-            inputs[2] = self._contains_food(x, y - 1)
-            inputs[3] = self._contains_poison(x - 1, y)
-            inputs[4] = self._contains_poison(x, y + 1)
-            inputs[5] = self._contains_poison(x, y - 1)
-        return inputs
-
-
-    def _move_forward(self):
-
-        # move one cell in the direction it is headed
-        if self.heading == 0:
-            # north
-            self.pos = (self.pos[0], (self.pos[1] - 1) % self.world.board_length)
-        elif self.heading == 1:
+        if direction == "n" or direction == 0:
+             # north
+            self.pos = (self.pos[0], (self.pos[1] - 1) % self.world.dimension[1])
+        elif direction == "e" or direction == 1:
             # east
-            self.pos = ((self.pos[0] + 1) % self.world.board_length, self.pos[1])
-        elif self.heading == 2:
+            self.pos = ((self.pos[0] + 1) % self.world.dimension[0], self.pos[1])
+        elif direction == "s" or direction == 2:
             # south
-            self.pos = (self.pos[0], (self.pos[1] + 1) % self.world.board_length)
-        elif self.heading == 3:
-            # west
-            self.pos = ((self.pos[0] - 1) % self.world.board_length, self.pos[1])
+            self.pos = (self.pos[0], (self.pos[1] + 1) % self.world.dimension[1])
+        elif direction == "w" or direction == 3:
+           # west
+            self.pos = ((self.pos[0] - 1) % self.world.dimension[0], self.pos[1])
 
-        self._check_for_food_and_poison()
+        self.steps_taken += 1
+
+        return self._check_for_food_and_poison()
 
     def _check_for_food_and_poison(self):
         # Check for food and poison, keep track of where agent has been
@@ -155,16 +76,14 @@ class Agent:
         if key in self.visited.keys():
             return
 
-        # board_val = self.board[self.pos[1]][self.pos[0]]
+        # Get content of agents position and return it
         board_val = self.world.get_cell_content(self.pos[0], self.pos[1])
-        if board_val == 1:
-            self.food_eaten += 1
-        elif board_val == 2:
-            self.poison_eaten += 1
-
         self.visited[key] = True
+        return board_val
 
-    def _contains_food(self, x, y): # TODO not tested
+    # TODO: REMOVE THESE? --------------------------------------------------------
+    def _contains_food(self, x, y):
+        # Only used internally, and on cells not visited
         key = str(x) + "-" + str(y)
         if key in self.visited.keys():
             return 0
@@ -172,7 +91,8 @@ class Agent:
             return 1
         return 0
 
-    def _contains_poison(self, x, y): # TODO not tested
+    def _contains_poison(self, x, y):
+        # Only used internally, and on cells not visited
         key = str(x) + "-" + str(y)
         if key in self.visited.keys():
             return 0
